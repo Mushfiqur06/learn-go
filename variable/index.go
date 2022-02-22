037a6d95a7b420fd7ac624707432e34c680a6b6c
m.niloy@cheq.io <mushfiqur06@gmail.com>
2022-02-22 11:10:57 +0600

    Learn Variable

---
+++ variable/index.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 5
+	fmt.Println("A", a)
+
+	var b, c int = 1, 2
+	fmt.Println("B & C", b, c)
+
+	var d = true
+	fmt.Println("D", d)
+
+	var e int
+	fmt.Println("E", e)
+
+	f := "Hello"
+	fmt.Println("F", f)
+}
